978ba089596b174d58d7352374ef6058fbb987de
buebos <98134814+buebos@users.noreply.github.com>
2023-04-25 17:18:22 -0700

    changes

--- practice_9.py
+++ practice_9.py
@@ -13,18 +13,26 @@ def error(real: float, experimental: float):
 # [0]: 'number_of_terms'
 # [1]: 'experimental_result'
 # [2]: 'error_percentage'
-table_results = [[], [], []]
+results_1 = [[], [], []]
 
 clear()
 
 xi = float(input("Type x inferior: "))
 xs = float(input("Type x superior: "))
 while xi > xs:
-    xs = float(input("Type x superior again, be sure to not type a number greater than the x inferior: "))
+    xs = float(
+        input(
+            "Type x superior again, be sure to not type a number greater than the x inferior: "
+        )
+    )
 
 step = float(input("Type the step between each x iteration: "))
 while step < 0:
-    step = float(input("Step must be a number greater than 0, type it again please: "))
+    step = float(
+        input(
+            "Step must be a number greater than 0, type it again please: "
+        )
+    )
 
 x = xi
 n = int(input("Type n: "))
@@ -34,16 +42,21 @@ while x <= xs:
     for i in range(0, n + 1):
         current_result = x**i / math.factorial(i)
         ex += current_result
-    table_results[0].append(x)
-    table_results[1].append(ex)
-    table_results[2].append(error(math.exp(x), ex))   
+    results_1[0].append(x)
+    results_1[1].append(ex)
+    results_1[2].append(error(math.exp(x), ex))
     x += step
 
 
-plt.plot(table_results[0], table_results[1], color="red", linewidth=2)
+plt.plot(results_1[0], results_1[1], color="red", linewidth=2)
 plt.grid()
 
-print(tabulate(transpose(table_results), ["Exponential", "Experimental result", "Error percentage %"]))
+print(
+    tabulate(
+        transpose(results_1),
+        ["Exponential", "Experimental result", "Error percentage %"],
+    )
+)
 
 x = int(input("Type x to raise Euler: "))
 n = int(input("Type n: "))
@@ -57,7 +70,11 @@ for i in range(n + 1):
     results_2[1].append(sum_ex)
     results_2[2].append(error(math.exp(x), sum_ex))
 
-
-print(tabulate(transpose(results_2), ["Terms", "Sum with term amount", "Error percentage %"]))
+print(
+    tabulate(
+        transpose(results_2),
+        ["Terms", "Sum with term amount", "Error percentage %"],
+    )
+)
 
 # %%
